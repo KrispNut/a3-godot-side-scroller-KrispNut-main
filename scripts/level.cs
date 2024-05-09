872e16e31544145e6c35b1204b70460e4db69bfb
KrispNut <sarimrabbi@gmail.com>
2024-05-09 14:47:31 -0700

    Trying level change

--- scripts/level.cs
+++ scripts/level.cs
@@ -3,20 +3,16 @@ using System;
 
 public partial class level : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// on body entered
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));		
 		GD.Print("level script ready.");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
 
-	// on body entered
 	private void OnBodyEntered(Node body)
 	{
 		GD.Print("+1");
